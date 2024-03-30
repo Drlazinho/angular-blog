612ad352d29a57e36423f513e64506027c5d0860
Lázaro Pimentel <118478127+LazaroAmvox@users.noreply.github.com>
2024-03-30 00:12:25 -0300

    Rota Dinamica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,19 +1,19 @@
 export const dataFake = [
     {
-        id: 1,
+        id: '1',
         title: "Livro: O Senhor dos Anéis",
         description: "Uma obra-prima da literatura fantástica, escrita por J.R.R. Tolkien. A história segue a jornada de Frodo Bolseiro, um hobbit que deve destruir o Um Anel, um artefato maligno que ameaça a Terra-média.",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a8/The_Lord_of_the_Rings_book_cover.jpg/220px-The_Lord_of_the_Rings_book_cover.jpg"
       },
       {
-        id: 2,
+        id: '2',
         title: "Filme: O Poderoso Chefão",
         description: "Um clássico do cinema americano, dirigido por Francis Ford Coppola. O filme conta a história da família Corleone, um clã mafioso de Nova York.",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/The_Godfather_poster.jpg/220px-The_Godfather_poster.jpg"
       },
       // ... outros objetos
       {
-        id: 3,
+        id: '3',
         title: "Ideia: Igualdade",
         description: "Um princípio fundamental da justiça social, que significa que todos os seres humanos são iguais em direitos e dignidade.",
         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Equality_symbol.svg/220px-Equality_symbol.svg.png"
